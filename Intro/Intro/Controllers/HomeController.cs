8b9374a35c44759a706e05f082e1496ccf39412d
Fernando <fmti91@hotmail.com>
2018-02-22 11:41:00 -0300

    Class 18, 19 and 20: Typed view and new form with POST method.

--- Intro/Intro/Controllers/HomeController.cs
+++ Intro/Intro/Controllers/HomeController.cs
@@ -17,22 +17,34 @@ namespace Intro.Controllers
             };
 
             //Example ViewData[ ]
-            ViewData["PersonId"] = person.PersonId;
-            ViewData["Name"] = person.Name;
-            ViewData["Type"] = person.Type;
+            //ViewData["PersonId"] = person.PersonId;
+            //ViewData["Name"] = person.Name;
+            //ViewData["Type"] = person.Type;
 
             //Example ViewBag
-            ViewBag.id = person.PersonId;
-            ViewBag.name = person.Name;
-            ViewBag.type = person.Type;
+            //ViewBag.id = person.PersonId;
+            //ViewBag.name = person.Name;
+            //ViewBag.type = person.Type;
 
-            return View(); // return > Views > Home > Index.chstml
+            return View(person); // return > Views > Home > Index.chstml
         }
 
         //new view Contacts (Views > Home > Contacts)
         public ActionResult Contacts()
         {
             return View(); // return > Views > Home > Contacts.cshtml
+        }
+
+        //new view Contacts (Views > Home > Contacts)
+        [HttpPost] //http post reference
+        public ActionResult List(int PersonId, string Name, string Type)
+        {
+            //ViewData
+            ViewData["PersonId"] = PersonId;
+            ViewData["Name"] = Name;
+            ViewData["Type"] = Type;
+
+            return View(); // return > Views > Home > List.cshtml
         }
     }
 }
