337adf1ccd941101f68c791949fa1b0f2a07b402
Fernando <fmti91@hotmail.com>
2018-02-27 17:24:21 -0300

    Class 25 and 26: Asp.Net validation, new class User.cs (Models), new Controller UserController and View Result.

---
+++ Intro/Intro/Controllers/UserController.cs
@@ -0,0 +1,36 @@
+﻿using Intro.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Intro.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Index()
+        {
+            var user = new User();
+            return View(user); //return user view
+        }
+
+        [HttpPost]
+        public ActionResult Index(User user)
+        {
+            //validation
+            if (ModelState.IsValid)
+            { //if true
+                return View("Result", user);
+            } //if false
+            return View(user);
+        }
+
+        public ActionResult Result(User user)
+        {
+            return View(user);
+        }
+
+    }
+}
---
+++ Intro/Intro/Models/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Intro.Models
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public string Observation { get; set; }
+        public string Age { get; set; }
+        public string Mail { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public string RepeatPassword { get; set; }
+    }
+}
